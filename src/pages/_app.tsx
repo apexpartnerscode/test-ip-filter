4311598bf2b0a49b7db169ddb203c3c9006b57fb
Eriquy Jhordan <eriquy@gmail.com>
2023-07-13 13:05:49 -0300

    feat: add existent code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
+import { OperationsStoreProvider } from '@/store'
 import '@/styles/globals.sass'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <OperationsStoreProvider><Component {...pageProps} /></OperationsStoreProvider>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,64 @@
-import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { useEffect, useState } from 'react';
 import { GetServerSideProps } from 'next'
+import { Inter } from 'next/font/google'
+import { supabase } from '../../supabase/supabaseClient'
+import { LoadingOutlined } from '@ant-design/icons';
+import { Spin } from 'antd';
+import styles from '../styles/home.module.sass'
+import { useRouter } from 'next/router';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter()
+  const [userSession, setUserSession] = useState(null)
+  const [isLoading, setIsLoading] = useState(false);
+  async function handleLogInAzure() {
+    try {
+      setIsLoading(true)
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'azure',   
+      },)
+    } catch (error) {
+        console.log(error)
+        return
+    }
+  }
+
   return (
     <>
-      <h1>ok</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.leftBox}>
+          <h1 className={styles.title}>Controle Câmbio</h1>
+          <div className={styles.image}>
+            <Image
+              src="/images/apxazulbranco.png"
+              alt="Logo APX azul PNG"
+              className={styles.apxLogo}
+              width={464}
+              height={96}
+            />
+          </div>
+          <div className={styles.footer}>
+            <p className={styles.version}>v1.0.0</p>
+            <p className={styles.userName}></p>
+          </div>
+        </div>
+        <div className={styles.rightBox}>
+          <h2 className={styles.rightTitle}>Operacional</h2>
+          <button onClick={handleLogInAzure} className={styles.button} type="button">
+            {
+              isLoading ? (
+                <Spin indicator={<LoadingOutlined style={{ fontSize: 24, color: '#fff' }} spin />} />
+              ) : (
+                "Início"
+              )
+            }
+          </button>
+          <div></div>
+        </div>
+      </div>
     </>
   )
 }
@@ -17,7 +66,7 @@ export default function Home() {
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const allowedIps = ['177.39.236.1'] as any
   const ip = ctx.req.headers['x-forwarded-for'] || ctx.req.socket.remoteAddress
-  if (allowedIps.includes(ip)) {
+  if (!allowedIps.includes(ip)) {
     return {
       redirect: {
         destination: '/404',
